4fd09c9af2a25f3f53c9c91fa7f2775f8679ec7a
vituu69 <victorcustodio845@gmail.com>
2024-08-21 20:58:37 -0300

    ultimos ajustes da api com docker compose

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
-from src.init import User_repo
+from .src.init import User_repo
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello_word():
     return 'ola estou na aplicacao setada'
 
-@app.route("/insert", methods=["GET"])
+@app.route("/insert", methods=["POST"])
 def insert():
     user_repo = User_repo()
     body = request.json
@@ -14,3 +14,6 @@ def insert():
     user_repo.insert_name(body['name'])
 
     return 'ok'
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- src/entities/users.py
+++ src/entities/users.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Column, String, Integer
+from sqlalchemy.sql.expression import true
 from src.configs import base
 
-class Users[base]:
+class Users(base):
 
     __tablename__ = 'users'
 
-    id = Column(Integer, pryimary_key=True)
+    id = Column(Integer, pryimary_key=true)
     name = Column(String)
 
     def __repr__(self) -> str:
